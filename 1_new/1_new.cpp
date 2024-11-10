acb4918483be2201c3ad565313317925cd348adf
Maks <rubelmaksum2404@gmail.com>
2024-11-10 16:40:56 +0200

    new

--- 1_new/1_new.cpp
+++ 1_new/1_new.cpp
@@ -6,7 +6,9 @@ using namespace std;
 
 int main()
 {
-    Fwqlist<int> list;
+	Fwqlist<int> list;
+	Fwqlist<int> list2;
+
 	for (size_t i = 0; i < 5; i++)
 	{
 		list.addHead(rand() % 50);
@@ -28,6 +30,20 @@ int main()
 
 	list.addTail(rand() % 50);
 	list.print();
+
+
+	list2.clone(list);
+	list.print();
+	list2.print();
+	list.addTail(rand() % 50);
+	list.print();
+	list2.print();
+
+
+	Fwqlist<int> list3 = list + list2;
+	list3.print();
+	Fwqlist<int> list4 = list * list2;
+	list4.print();
 
 }
 
--- 1_new/Fwqlist.h
+++ 1_new/Fwqlist.h
@@ -1,3 +1,4 @@
+ï»¿
 #pragma once
 #include <iostream>
 using namespace std;
@@ -6,12 +7,12 @@ template<typename T>
 struct Node
 {
 	T data;
-	Node * next;
+	Node* next;
 	Node(const T& data = T(), Node* next = nullptr)
-		:data(data), next(next){}
+		:data(data), next(next) {}
 
 
-	
+
 };
 template<typename T>
 
@@ -21,14 +22,16 @@ public:
 	Fwqlist() = default;
 	bool empty() const;
 	void print() const;
-	
+
 	~Fwqlist();
 
 	void addHead(const T& data);
 	void addTail(const T& data);
 	void removeHead();
 	void clear();
-
+	void clone(const Fwqlist<T>& other);
+	Fwqlist<T> operator+(const Fwqlist<T>& other) const;
+	Fwqlist<T> operator*(const Fwqlist<T>& other) const;
 
 private:
 	Node<T>* head = nullptr;
@@ -53,12 +56,12 @@ inline void Fwqlist<T>::print() const
 		cout << "List is empty" << endl;
 		return;
 	}
-	auto tmp = head; // ïî÷èíàºìî ç ïî÷àòêó ñïèñêó
+	auto tmp = head; // Ã¯Ã®Ã·Ã¨Ã­Ã ÂºÃ¬Ã® Ã§ Ã¯Ã®Ã·Ã Ã²ÃªÃ³ Ã±Ã¯Ã¨Ã±ÃªÃ³
 	cout << "List: \t";
-	while (tmp != nullptr) // âèâîäèìî äîêè íå äîéøëè äî ê³íöÿ
+	while (tmp != nullptr) // Ã¢Ã¨Ã¢Ã®Ã¤Ã¨Ã¬Ã® Ã¤Ã®ÃªÃ¨ Ã­Ã¥ Ã¤Ã®Ã©Ã¸Ã«Ã¨ Ã¤Ã® ÃªÂ³Ã­Ã¶Ã¿
 	{
 		cout << tmp->data << "\t";
-		tmp = tmp->next; // ïåðåõîäèìî äî íàñòóïíîãî âóçëà
+		tmp = tmp->next; // Ã¯Ã¥Ã°Ã¥ÃµÃ®Ã¤Ã¨Ã¬Ã® Ã¤Ã® Ã­Ã Ã±Ã²Ã³Ã¯Ã­Ã®Ã£Ã® Ã¢Ã³Ã§Ã«Ã 
 	}
 	cout << endl;
 
@@ -121,4 +124,65 @@ inline void Fwqlist<T>::clear()
 	}
 	tail = nullptr;
 	size = 0;
+}
+
+template<typename T>
+inline void Fwqlist<T>::clone(const Fwqlist<T>& other)
+{
+	clear();
+
+	if (other.head == nullptr) {
+		head = nullptr;
+		return;
+	}
+	head = new Node<T>(other.head->data);
+	Node<T>* tmp = head;
+	Node<T>* tmp_other = other.head->next;
+
+	while (tmp_other != nullptr) {
+		tmp->next = new Node<T>(tmp_other->data);
+		tmp = tmp->next;
+		tmp_other = tmp_other->next;
+	}
+	
+}
+
+template<typename T>
+inline Fwqlist<T> Fwqlist<T>::operator+(const Fwqlist<T>& other) const
+{
+	Fwqlist<T> result;
+	auto tmp = head;
+	while (tmp != nullptr)
+	{
+		result.addTail(tmp->data);
+		tmp = tmp->next;
+	}
+	auto other_tmp = other.head;
+	while (other_tmp != nullptr)
+	{
+		result.addTail(other_tmp->data);
+		other_tmp = other_tmp->next;
+	}
+	return result;
+}
+
+template<typename T>
+inline Fwqlist<T> Fwqlist<T>::operator*(const Fwqlist<T>& other) const
+{
+	Fwqlist<T> result;
+	auto tmp = head;
+	while (tmp != nullptr)
+	{
+		auto other_tmp = other.head;
+		while (other_tmp != nullptr)
+		{
+			if (tmp->data == other_tmp->data) {
+				result.addTail(tmp->data);
+				break;
+			}
+			other_tmp = other_tmp->next;
+		}
+		tmp = tmp->next;
+	}
+	return result;
 }
